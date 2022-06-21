084dae3b7e3fe6295e8bbfd041d6ec86803334d8
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-06-21 08:51:57 +0300

    Update script.js file

--- script/script.js
+++ script/script.js
@@ -1,2 +1,5 @@
 const menu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+menu.addEventListener('click', () => {
+  mobileMenu.classList.remove('display-mobile');
+});

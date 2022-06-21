fe9202955dc4dd22c36e29e01cd0053ed143d7be
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-06-20 23:32:12 -0700

    add javascript file

--- script/script.js
+++ script/script.js
@@ -3,3 +3,8 @@ const mobileMenu = document.querySelector('.mobile-menu');
 menu.addEventListener('click', () => {
   mobileMenu.classList.remove('display-mobile');
 });
+
+const closeIcon = document.querySelector('header .mobile-menu .close-menu a');
+closeIcon.addEventListener('click', () => {
+  mobileMenu.classList.add('display-mobile');
+});

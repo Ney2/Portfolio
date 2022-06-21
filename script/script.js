0dc03b129758687c2c659abe260bff74ba354708
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-06-21 10:14:08 +0300

    Update script.js file

--- script/script.js
+++ script/script.js
@@ -3,8 +3,13 @@ const mobileMenu = document.querySelector('.mobile-menu');
 menu.addEventListener('click', () => {
   mobileMenu.classList.remove('display-mobile');
 });
-
 const closeIcon = document.querySelector('header .mobile-menu .close-menu a');
 closeIcon.addEventListener('click', () => {
   mobileMenu.classList.add('display-mobile');
 });
+const links = document.querySelectorAll('header .mobile-menu ul li a');
+for (let i = 0; i < links.length; i += 1) {
+  links[i].addEventListener('click', () => {
+    mobileMenu.classList.add('display-mobile');
+  });
+}

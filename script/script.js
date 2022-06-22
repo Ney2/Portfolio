1fdc78e013a502a047161ae310f7388b22814cdd
Mehria Rahmani <mehriarahmani4@gmail.com>
2022-06-22 00:57:22 -0700

    update the script file

--- script/script.js
+++ script/script.js
@@ -34,4 +34,26 @@ const projects = [
       modalContainer: 'modal-container1',
       modalCloseBtn: 'modal-close-btn1',
     },
-  }
+  },
+
+  {
+    title: 'Professional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry´s standard',
+    technologies: ['html', 'bootstrap', 'css'],
+    details:
+              `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s 
+              with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+              standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax 
+              map lapora verita.
+              `,
+    LiveLink: '',
+    SourceLink: '',
+    image: './img/snapshoot.png',
+    privateClasses: {
+      seeProjectBtn: 'see-projec-btn-1',
+      modalContainer: 'modal-container1',
+      modalCloseBtn: 'modal-close-btn1',
+    },
+  ]
+
